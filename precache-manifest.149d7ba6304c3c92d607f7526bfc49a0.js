e7914c5efa97691acb8b8925ff364c158bdfa2ed
Yetian <yetian.mao@gmail.com>
2018-10-25 03:29:29 +0900

    Updates

---
+++ precache-manifest.149d7ba6304c3c92d607f7526bfc49a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0610d786c7f4d64072c",
+    "url": "/static/css/main.f260c620.chunk.css"
+  },
+  {
+    "revision": "c0610d786c7f4d64072c",
+    "url": "/static/js/main.c0610d78.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1553225f5b22c811127b3f579c4ca2ae",
+    "url": "/index.html"
+  }
+];
